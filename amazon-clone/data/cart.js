e5a75744882c7d5e045db4fc97d905ab31927252
dvsasks <dvsask.sharma@gmail.com>
2025-05-07 01:02:04 +0530

    delete of cart items functionality in checkout page

--- amazon-clone/data/cart.js
+++ amazon-clone/data/cart.js
@@ -1,13 +1,16 @@
-export const cart = [
-  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1,
-  },
-  {
-    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2,
-  },
-];
+let oldCart = JSON.parse(window.localStorage.getItem("cart"));
+export let cart = oldCart
+  ? oldCart
+  : [
+      // {
+      //   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      //   quantity: 1,
+      // },
+      // {
+      //   id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      //   quantity: 2,
+      // },
+    ];
 export function addToCart() {
   document.querySelectorAll(".js-add-to-cart").forEach((button) => {
     button.addEventListener("click", () => {
@@ -28,15 +31,16 @@ export function addToCart() {
       }
       /* for updating cart bage in amazon homepage */
       document.querySelector(".js-cart-quantity").innerHTML = noOfCartItems();
-      console.log(cart);
+      window.localStorage.setItem("cart", JSON.stringify(cart));
     });
   });
 }
 
 export function noOfCartItems() {
-  let itemsCount = 0;
-  cart.forEach((cartItem) => {
-    itemsCount = itemsCount + cartItem.quantity;
-  });
-  return itemsCount;
+  return cart.reduce((acc, cur) => (acc = acc + cur.quantity), 0);
+}
+
+export function removeCartItem(productId) {
+  cart = cart.filter((cartItem) => cartItem.productId !== productId);
+  window.localStorage.setItem("cart", JSON.stringify(cart));
 }
